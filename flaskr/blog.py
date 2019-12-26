36b7c889cc985c32545eaf7d2f2e08ac11de85a6
crowix <mdzhangkailai@126.com>
2019-12-26 17:35:02 +0800

    showdown added

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -29,6 +29,16 @@ def hehe():
     ).fetchall()
     return render_template('blog/simple.html', posts=posts)
 
+@bp.route('/hehe2')
+def hehe2():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/html2.html', posts=posts)
+
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
