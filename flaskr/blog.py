b8f0718cc3ab7db68329d8c61c924821477749a0
crowix <mdzhangkailai@126.com>
2019-12-27 19:14:48 +0800

    js splited

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -19,16 +19,6 @@ def index():
     return render_template('blog/index.html', posts=posts)
 
 
-@bp.route('/hehe')
-def hehe():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/simple.html', posts=posts)
-
 @bp.route('/hehe2')
 def hehe2():
     db = get_db()
