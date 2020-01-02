99908d14a3e7776f73e35d91345b949c4502ff17
crowix <mdzhangkailai@126.com>
2020-01-02 20:48:20 +0800

    catalog finished

--- flaskr/static/js/trans.js
+++ flaskr/static/js/trans.js
@@ -9,20 +9,75 @@ let converter = new showdown.Converter(),
 let translation = (function () {
     return function () {
         let temp = converter.makeHtml(container_edit.value);
+        temp = temp.replace(/<h1 id="">/, "<h1 id=\"0\">")
         container_show.innerHTML = temp;
         container_catalog.innerHTML = makeCatalogs(temp);
         // 按比例调整 预览容器 的滚动条位置
         let ratio = container_edit.scrollTop / container_edit.scrollHeight;
         container_show.scrollTop = container_show.scrollHeight * ratio;
+        container_catalog.scrollTop = container_catalog.scrollHeight * ratio;
     }
 })();
 
 
 makeCatalogs = function (text) {
-    text = text.replace(/h1/g, "a");
-    text = text.replace(/id="/g, "href=\"#");
-    // let hehe2 = text.match(/h1 /);
-    return text;
+    let newText = "<dl>"
+    let temp = ""
+    let index = 0
+    text = text.replace(/<h1 id="">/, "<h1 id=\"0\">")
+    // while (true) {
+    //
+    // }
+    // text = text.replace(/<h1 id/, "dtttt");
+    // text = text.replace(/<h1*<\/h1>/, "dt");
+    // text = text.replace(/h2/g, "dd");
+    // text = text.replace(/id="/g, "href=\"#");
+
+    while (true) {
+
+        temp = text.match(/<h[12].*?<\/h[12]>/);
+        index = text.search(/<h[12].*?<\/h[12]>/);
+        if (temp == null) {
+            break
+        } else {
+            if (text.search(/<h1.*?<\/h1>/) == index) {
+                temp = "<dt>" + temp + "</dt>"
+                temp = temp.replace(/h1 id="/, "a href=\"#")
+                newText += temp
+            } else {
+                temp = "<dd>" + temp + "</dd>"
+                temp = temp.replace(/h2 id="/, "a href=\"#")
+                newText += temp
+            }
+            text = text.replace(/<h[12].*?<\/h[12]>/, "")
+        }
+    }
+
+    // while (true) {
+    //     temp = text.match(/<h1.*?<\/h1>/);
+    //     text = text.replace(/<h1.*?<\/h1>/, "")
+    //     if (temp == null) {
+    //         temp = text.match(/<h2.*?<\/h2>/);
+    //         text = text.replace(/<h2.*?<\/h2>/, "")
+    //         if (temp == null) {
+    //             break
+    //         } else {
+    //             temp = "<dd>" + temp + "</dd>"
+    //             temp = temp.replace(/h2 id="/, "a href=#\"")
+    //             newText += temp
+    //         }
+    //     } else {
+    //         temp = "<dt>" + temp + "</dt>"
+    //         temp = temp.replace(/h1 id="/, "a href=#\"")
+    //         newText += temp
+    //     }
+    // }
+
+
+    // text = text.match(/<\/h1>/);
+
+    newText += "</dl>"
+    return newText;
 };
 
 
@@ -44,6 +99,7 @@ container_edit.onmouseleave = function () {
 //监听鼠标进入 预览容器范围 设置状态
 container_show.onmouseover = function () {
     container_edit.classList.remove('cur')
+    container_catalog.classList.remove('cur')
     container_show.classList.add('cur');
 };
 //监听鼠标离开 预览容器范围 设置状态
@@ -59,6 +115,7 @@ container_edit.addEventListener('scroll', function () {
         container_show.scrollTop = container_show.scrollHeight * ratio;
     }
 });
+
 // 监听 预览容器 内容滚动
 container_show.addEventListener('scroll', function () {
     // 若鼠标在 预览范围内 则 以 预览容器为主 同步 输入框内容滚动条高度
